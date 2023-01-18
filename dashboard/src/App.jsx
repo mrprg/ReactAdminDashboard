11fe7d0f879b808f4c7a78527273be3b48ce1f01
mrprg <mohamadrezaasadi322@gmail.com>
2023-01-18 11:14:28 +0330

    test3

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      this a test div!!!!
+    </div>
+  )
+}
+
+export default App
+
+
+
+
