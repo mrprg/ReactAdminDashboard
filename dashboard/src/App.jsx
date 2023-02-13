396de353296c5991be5510e28e940c4204148db5
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-13 10:40:04 +0330

    APP.JSX SRTUCTURE

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,10 +1,40 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-import React from "react";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 const App = () => {
-  return <div>this a test div!!!!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div
+            className="fixed right-4 bottom-4"
+            style={{ zIndex: "1000" }}>
+            <TooltipComponent
+              content="Settings"
+              position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{
+                  background: "blue",
+                  borderRadius: "50%",
+                }}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
