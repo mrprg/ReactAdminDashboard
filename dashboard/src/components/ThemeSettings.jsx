0438a7c1dfec9ea9a0fce1efb5c564961ec1cbfb
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-14 22:08:09 +0330

    fix a typo mistake

--- dashboard/src/components/ThemeSettings.jsx
+++ dashboard/src/components/ThemeSettings.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ThemeSetting = () => {
+const ThemeSettings = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const ThemeSetting = () => {
   )
 }
 
-export default ThemeSetting
+export default ThemeSettings
