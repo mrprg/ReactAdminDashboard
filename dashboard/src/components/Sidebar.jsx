943bcc40073ebd157a0091d746e2198a82473324
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-18 00:22:36 +0330

    SideBar Styling and Structure

--- dashboard/src/components/Sidebar.jsx
+++ dashboard/src/components/Sidebar.jsx
@@ -3,10 +3,12 @@ import { Link, NavLink } from "react-router-dom";
 import { SiShopware } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import { link } from "../data/dummy";
+import { link, links } from "../data/dummy";
 
 const Sidebar = () => {
   const activeMenu = true;
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
@@ -29,6 +31,26 @@ const Sidebar = () => {
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
+          </div>
+          <div className="mt-10">
+            {links.map((item) => (
+              <div key={item.title}>
+                <p className="text-gray-400 m-3 mt-4 uppercase">
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
+                  <NavLink to={`/${link.name}`}
+                    key={link.name}
+                    onClick={(() => {})}
+                    className={({ isActive }) => isActive ? activeLink : normalLink}
+                  >
+                    {link.icon}
+                    <span className="capitalize">{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+
+            ))}
           </div>
         </>
       )}
