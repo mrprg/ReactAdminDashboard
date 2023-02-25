57db60d3d3febc67ce92cb31c5dd539387220998
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-25 23:11:36 +0330

    Orders Component

--- dashboard/src/pages/Orders.jsx
+++ dashboard/src/pages/Orders.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import {
+  ordersData,
+  contextMenuItems,
+  ordersGrid,
+} from "../data/dummy";
+import Header from "../components/Header";
 
 const Orders = () => {
   return (
-    <div>
-      
+    <div className=" m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
     </div>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
