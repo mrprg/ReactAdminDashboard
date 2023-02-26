4636b3ad27d61beccd37293aa0712371a527ad29
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-26 22:17:03 +0330

    Editor App

--- dashboard/src/pages/Editor.jsx
+++ dashboard/src/pages/Editor.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+import { Header } from "../components";
 const Editor = () => {
   return (
-    <div>
-      
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 text-white rounded-3xl">
+      <RichTextEditorComponent>
+        <Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]} />
+      </RichTextEditorComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Editor
+export default Editor;
