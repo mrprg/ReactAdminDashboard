efdc4055c1794f678e0a721419fc2e34fbd8a102
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-26 21:18:38 +0330

    App section / Calender App

--- dashboard/src/pages/Calendar.jsx
+++ dashboard/src/pages/Calendar.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
+
+import { scheduleData } from "../data/dummy";
+import { Header } from "../components";
 
 const Calendar = () => {
   return (
-    <div>
-      
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Calender" category="App" />
+      <ScheduleComponent
+        height="650px"
+        selectedDate={new Date(2021, 0, 10)}
+        eventSettings={{ dataSource: scheduleData }}>
+        <Inject
+          services={[
+            Day,
+            Week,
+            WorkWeek,
+            Month,
+            Agenda,
+            Resize,
+            DragAndDrop,
+          ]}
+        />
+      </ScheduleComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Calendar
+export default Calendar;
