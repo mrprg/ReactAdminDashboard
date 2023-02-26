6104a6e7e712b6cf5c5d21c74fc12414a98e02e5
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-26 20:08:11 +0330

    Employees Page

--- dashboard/src/pages/Employees.jsx
+++ dashboard/src/pages/Employees.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Search,
+  Page,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+
+import {
+  employeesData,
+  employeesGrid,
+} from "../data/dummy";
+import Header from "../components/Header";
 
 const Employees = () => {
   return (
-    <div>
-      
+    <div className=" m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowSorting
+        allowPaging
+        toolbar={["Search"]}
+        width="auto">
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Employees
+export default Employees;
