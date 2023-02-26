f574bf629def1d32aeb4fe960c781c73fec926d3
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-26 21:55:30 +0330

    Kanban App

--- dashboard/src/pages/Kanban.jsx
+++ dashboard/src/pages/Kanban.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
+import { kanbanData, kanbanGrid } from "../data/dummy";
+import { Header } from "../components";
 const Kanban = () => {
   return (
-    <div>
-      sddsds
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Kanban" />
+      <KanbanComponent
+        dataSource={kanbanData}
+        id="kanban"
+        cardSettings={{
+          contentField: "Summary",
+          headerField: "Id",
+        }}
+        keyField="Status">
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Kanban
+export default Kanban;
