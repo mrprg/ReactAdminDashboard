57d2fb60f84ddd9e9b514ba0f87317c5d5120db1
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-26 18:43:35 +0330

    Finish Orders Page

--- dashboard/src/pages/Orders.jsx
+++ dashboard/src/pages/Orders.jsx
@@ -25,6 +25,29 @@ const Orders = () => {
   return (
     <div className=" m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowSorting
+        allowPaging>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
     </div>
   );
 };
