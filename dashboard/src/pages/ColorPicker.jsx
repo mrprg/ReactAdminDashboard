2e3c082ae715e5c18a2a5d56c5dfb3411b5c3bdd
mrprg <mohamadrezaasadi322@gmail.com>
2023-02-27 19:49:04 +0330

    Color Picker App

--- dashboard/src/pages/ColorPicker.jsx
+++ dashboard/src/pages/ColorPicker.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
+import { Header } from "../components";
 
-const ColorPicker = () => {
-  return (
-    <div>
-      
+const change = (args) => {
+  document.getElementById("preview").style.backgroundColor =
+    args.currentValue.hex;
+};
+const CustomColorPicker = ({ id, mode }) => (
+  <ColorPickerComponent
+    inline
+    showButtons={false}
+    modeSwitcher={false}
+    id={id}
+    mode={mode}
+    change={change}
+  />
+);
+
+const ColorPicker = () => (
+  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="App" title="Color Picker" />
+    <div className="text-center">
+      <div id="preview" />
+      <div className="flex justify-center items-center gap-20 flex-wrap">
+        <div>
+          <p className="text-2xl font-semibold mt-2 mb-4">
+            Inline Pallete
+          </p>
+          <CustomColorPicker
+            id="inline-palette"
+            mode="Palette"
+          />
+        </div>
+        <div>
+          <p className="text-2xl font-semibold mt-2 mb-4">
+            Inline Picker
+          </p>
+          <CustomColorPicker
+            id="inline-picker"
+            mode="Picker"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
-
-export default ColorPicker
+  </div>
+);
+export default ColorPicker;
